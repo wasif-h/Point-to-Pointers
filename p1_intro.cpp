74844a558b5ef76d9f1d846fde4c3a7ad821e065
Wasif Hossain <wasif.hx@gmail.com>
2024-12-24 22:35:00 +0600

    p1, p2 added

---
+++ p1_intro.cpp
@@ -0,0 +1,39 @@
+// Author : Wasif Hossain <https://www.linkedin.com/in/wasif-h>
+// Topic  : Pointer                                                 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// <<<---------  main function   --------->>> 
+int main()
+{
+
+    cout << "---- Code with Wasif ----\n\n";
+    cout << "------int pointer -------"<<endl;
+
+    int a = 5;
+    int *ptr;
+    ptr = &a;
+    cout << a << endl;
+    cout << ptr << endl;
+    cout << &a << endl;
+    cout << &ptr << endl;
+    // dereferencing
+    cout << *ptr << endl;
+
+    cout << "------char pointer -------"<<endl;
+
+    char b = 'f';
+    char *pchr;
+    pchr = &b;
+    cout << b << endl;
+    cout << pchr << endl;
+    cout << &b << endl;
+    cout << &pchr << endl;
+    // dereferencing
+    cout << *pchr << endl;
+
+
+
+    return 0;
+}
---
+++ p2_dereferencing.cpp
@@ -0,0 +1,31 @@
+// Author : Wasif Hossain <https://www.linkedin.com/in/wasif-h>
+// Topic  : dereferencing
+#include <bits/stdc++.h>
+using namespace std;
+
+// <<<---------  main function   --------->>>
+int main()
+{
+
+    cout << "---- Code with Wasif ----\n\n";
+
+    // ptr -> address
+    // *ptr -> value stored in that address
+
+    int a = 10;
+    int *ptr = &a;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    int b = 2;
+    *ptr = b; // it will change the value of a but not the address
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    // to change the address stored in ptr
+    ptr = &b;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    return 0;
+}
